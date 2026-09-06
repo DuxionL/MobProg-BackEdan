c11a0575fd2f905cae9ce73fcdd58c54d21a4efe
Moureno <ahaibonsai@gmail.com>
2026-09-06 18:31:54 +0700

    feat(statistics): add statistics page header navigation

--- lib/features/Statistic/screens/statistic_page.dart
+++ lib/features/Statistic/screens/statistic_page.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import '../widgets/statistic_header_button.dart';
 
-class StatisticPage extends StatelessWidget {
+class StatisticPage extends StatefulWidget {
   const StatisticPage({super.key});
 
   @override
+  State<StatisticPage> createState() => _StatisticPageState();
+}
+
+class _StatisticPageState extends State<StatisticPage> {
+  bool isIncome = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Statistics"),
-      ),
+    return Column(
+      children: [
+
+        Row(
+          children: [
+
+            StatisticHeaderButton(
+              title: "Income",
+              amount: 10000,
+              selected: isIncome,
+              onTap: () {
+                setState(() {
+                  isIncome = true;
+                });
+              },
+            ),
+
+            StatisticHeaderButton(
+              title: "Expenses",
+              amount: 1064,
+              selected: !isIncome,
+              onTap: () {
+                setState(() {
+                  isIncome = false;
+                });
+              },
+            ),
+
+          ],
+        ),
+
+        const Divider(
+          height: 1,
+          color: Colors.grey,
+        ),
+
+        Expanded(
+          child: Center(
+            child: Text(
+              isIncome
+                  ? "Income Chart Here"
+                  : "Expense Chart Here",
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
